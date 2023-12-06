7c16019fcfbea045eff0ec8336fffc7eaeceb40f
mozhinag <manimozhinagooran06@gmail.com>
2023-12-07 01:40:49 +0530

    bugs

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,3 +1,5 @@
+// CartProvider.js
+
 import React, { createContext, useState } from "react";
 import { productDetails } from "./productDetails";
 
@@ -8,61 +10,69 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
 
+  const calculateTotal = (cartItems) => {
+    return cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  };
+
   const handleAddToCart = (productDetails) => {
     const existingItem = cart.find((item) => item.id === productDetails.id);
 
     if (existingItem) {
       const updatedCart = cart.map((item) =>
         item.id === existingItem.id ? { ...item, quantity: item.quantity + 1 } : item
+
       );
       setCart(updatedCart);
     } else {
       setCart([...cart, { ...productDetails, quantity: 1 }]);
     }
-
-    setTotal(total + parseFloat(productDetails.price));
+    setTotal(calculateTotal(cart));
+ 
+  
   };
 
   const handleRemoveItem = (item) => {
     alert(`Item ${item.name} is removed`);
     const newCart = cart.filter((cartItem) => cartItem.id !== item.id);
-    setTotal(total - item.price * item.quantity);
+    
     setCart(newCart);
+    setTotal(calculateTotal(newCart));
   };
 
-  
-const handleIncreaseQuantity = (item) => {
-  const updatedCart = cart.map((cartItem) => {
-    if (cartItem.id === item.id) {
-      return {
-        ...cartItem,
-        quantity: cartItem.quantity + 1,
-        price: cartItem.price * (cartItem.quantity + 1), 
-      };
-    }
-    return cartItem;
-  });
+  const handleIncreaseQuantity = (item) => {
+    const updatedCart = cart.map((cartItem) =>
+      cartItem.id === item.id
+        ? {
+            ...cartItem,
+            quantity: cartItem.quantity + 1,
+       
+            price: cartItem.price * (cartItem.quantity + 1),
+          }
+        : cartItem
+    );
+
     setCart(updatedCart);
-    setTotal(total + parseFloat(item.price));
+    setTotal(calculateTotal(updatedCart));
   };
 
   const handleDecreaseQuantity = (item) => {
     const updatedCart = cart.map((cartItem) => {
       if (cartItem.id === item.id) {
-        const newQuantity = Math.max(cartItem.quantity - 1, 0); 
+        const newQuantity = Math.max(cartItem.quantity - 1, 1);
         return {
           ...cartItem,
           quantity: newQuantity,
-          price: cartItem.price * newQuantity, 
+          
+          price: cartItem.price * newQuantity,
         };
       }
       return cartItem;
     });
   
-      setCart(updatedCart);
-      setTotal(total - parseFloat(item.price));
-    }
-
+    setCart(updatedCart);
+    setTotal(calculateTotal(updatedCart));
+  };
+  
 
   return (
     <CartContext.Provider
